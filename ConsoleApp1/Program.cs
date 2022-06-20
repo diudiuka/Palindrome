36e4d5ca4f43503017c1f13e715a911f9671031e
evgeny.kamiak <evgeny.kamiak@hiqo-solutions.com>
2022-06-20 21:13:19 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var text = Console.ReadLine();
+            int.TryParse(text, out int intText);
+
+            var result = new Solution().IsPalindrome(intText);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string xStr = x.ToString();
+            int stringLength = xStr.Length;
+            int countOfComparition = stringLength / 2;
+            for (int i = 0; i < countOfComparition; i++)
+            {
+                if (xStr[i] != xStr[stringLength - (i + 1)])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
